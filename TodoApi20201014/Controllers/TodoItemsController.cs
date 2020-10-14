2ece4cb2b01a53e290455754d431881b6c5a733f
pandajiny <astic1764@gmail.com>
2020-10-14 16:21:08 +0900

    create sample todolist for myself

---
+++ TodoApi20201014/Controllers/TodoItemsController.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi20201014.Models;
+using System.Linq;
+
+
+namespace TodoApi20201014.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoItemsController : ControllerBase
+    {
+        public readonly TodoContext _context;
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET : api/TodoItems
+        [HttpGet]
+        public List<TodoItem> GetTodoItems()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> PostTodoItem([FromBody] TodoItem todoItem)
+        {
+            System.Diagnostics.Debug.WriteLine(todoItem);
+
+            _context.Add(todoItem);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetTodoItems), new { id = todoItem.Id }, todoItem);
+        }
+        // [HttpPost]
+        // public TodoItem TestPostItem([FromBody] TodoItem todoItem)
+        // {
+        //     return todoItem;
+        // }
+
+        // GET: api/TodoItems/Greeting
+        [Route("[action]")]
+        public string Greeting()
+        {
+            return "hello";
+        }
+    }
+}
---
+++ TodoApi20201014/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using TodoApi20201014.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace TodoApi20201014
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+            .AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+            services.AddDbContext<TodoContext>(options
+             => options.UseSqlite(Configuration.GetConnectionString("TodoContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
